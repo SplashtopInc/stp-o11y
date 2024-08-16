71625e5da6c4d60572942aa6a9ddbf2380877d3a
Neil Kuan <guan840912@gmail.com>
2024-08-16 12:56:44 +0800

    fix: release-workflows

--- scripts/get-helm-depends-update.mjs
+++ scripts/get-helm-depends-update.mjs
@@ -1,39 +1,22 @@
 #!/usr/bin/env zx
-const ChartLock = YAML.parse(fs.readFileSync('./Chart.lock', 'utf8'));
-const ChartYaml = YAML.parse(fs.readFileSync('./Chart.yaml', 'utf8'));
+const ChartYaml = YAML.parse(fs.readFileSync('./charts/stp-o11y/Chart.yaml', 'utf8'));
 let repoList = {
-    ChartLock: [],
     ChartYaml: [],
 };
-ChartLock.dependencies.forEach( repo => {
-  repoList.ChartLock.push({repoName: repo.name, repository: repo.repository});
-});
-
 ChartYaml.dependencies.forEach( repo => {
   repoList.ChartYaml.push({repoName: repo.name, repository: repo.repository});
 });
 
-console.log(repoList)
-
-if (repoList.ChartYaml.length !== repoList.ChartLock.length) {
-    throw new Error(chalk.red(`Please run helm dependency update at local and commit Chart.yaml and Chart.lock`));
-}
-
 const { spawn } = require("child_process");
-for (const [index, lock] of repoList.ChartLock.entries()) {
+for (const [index, lock] of repoList.ChartYaml.entries()) {
     let helmAddRepo;
     if (lock.repository !== repoList.ChartYaml[index].repository){
-        if (!repoList.ChartYaml[index].repository.includes('oci://')){
-            helmAddRepo = spawn('helm', ['repo', 'add', '--force-update', repoList.ChartYaml[index].repository.split('@')[1], lock.repository]);
-        }
+        helmAddRepo = spawn('helm', ['repo', 'add', '--force-update', repoList.ChartYaml[index].repository.split('@')[1], lock.repository]);
     }else {
-        if (!repoList.ChartYaml[index].repository.includes('oci://')){
-            helmAddRepo = spawn('helm', ['repo', 'add', '--force-update', lock.repoName, lock.repository]);
-        }
+        helmAddRepo = spawn('helm', ['repo', 'add', '--force-update', lock.repoName, lock.repository]);
     }
     const helmActionList = [helmAddRepo];
-    if (helmAddRepo){
-        for (const action of helmActionList) {
+    for (const action of helmActionList) {
         action.stdout.on("data", data => {
            console.log(`stdout: ${data}`);
         });
@@ -45,6 +28,5 @@ for (const [index, lock] of repoList.ChartLock.entries()) {
         action.on("close", code => {
             console.log(`child process exited with code ${code}`);
         });
-        }
     }
 }
